0d3817157ea51a4c2eda67cf3525e06e82a4019c
SorenA <s.dev@lysdal.org>
2020-06-21 02:45:56 +0200

    Add test project.

---
+++ tests/LightOps.DependencyInjection.Test/Fixtures/ComponentFixture.cs
@@ -0,0 +1,20 @@
+﻿using LightOps.DependencyInjection.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LightOps.DependencyInjection.Test.Fixtures
+{
+    public class BasicComponentFixture
+    {
+        public ServiceProvider ServiceProvider { get; }
+
+        public BasicComponentFixture()
+        {
+            var services = new ServiceCollection();
+
+            // Add dependency injection component
+            services.AddLightOpsDependencyInjection();
+
+            ServiceProvider = services.BuildServiceProvider();
+        }
+    }
+}
---
+++ tests/LightOps.DependencyInjection.Test/Tests/ComponentTests.cs
@@ -0,0 +1,45 @@
+﻿using LightOps.DependencyInjection.Api.Providers;
+using LightOps.DependencyInjection.Configuration;
+using LightOps.DependencyInjection.Test.Fixtures;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace LightOps.DependencyInjection.Test.Tests
+{
+    public class ComponentTests : IClassFixture<BasicComponentFixture>
+    {
+        [Fact]
+        public void TestComponentIsAttached()
+        {
+            var services = new ServiceCollection();
+
+            // Add component
+            services.AddLightOpsDependencyInjection();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Get 
+            var dependencyInjectionComponentStateProvider = serviceProvider.GetService<IDependencyInjectionComponentStateProvider>();
+
+            Assert.Contains("lightops.dependency-injection",
+                dependencyInjectionComponentStateProvider.AttachedComponentNames);
+        }
+
+        [Fact]
+        public void TestComponentConfiguration_Invoked()
+        {
+            var services = new ServiceCollection();
+
+            // Add component
+            var invoked = false;
+            services.AddLightOpsDependencyInjection(component =>
+            {
+                invoked = true;
+            });
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            Assert.True(invoked);
+        }
+    }
+}
